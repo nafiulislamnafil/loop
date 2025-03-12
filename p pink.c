5f2848c12e683c839eced420d441eefe8b90b86e
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-12 11:24:36 +0600

    Update p pink.c

--- p pink.c
+++ p pink.c
@@ -7,3 +7,16 @@ for(i=2;i<=2*n;i+=2){
 }
 return 0;
 }
+another way===>
+
+#include <stdio.h>
+int main() {
+    int n, count = 0, sum = 0;
+    scanf("%d", &n);
+    while (count < n) {
+        printf("%d\n", sum);
+        sum += 2;
+        count++;
+    }
+    return 0;
+}

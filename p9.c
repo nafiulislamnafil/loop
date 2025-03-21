c7d2414f370b84718919a9bd81f3be621c4178c0
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-22 01:45:36 +0600

    Update p9.c

--- p9.c
+++ p9.c
@@ -1,3 +1,28 @@
+#include<stdio.h>
+int main(){
+int i,m,n;
+scanf ("%d%d",&m,&n);
+if(m<n){
+        i=m+1;
+  while(i<n){
+     if(i%5==2||i%5==3){
+         printf("%d\n",i);
+      }
+    i++;
+   }
+}
+else if(n<m){
+        i=n+1;
+  while(i<m){
+        if(i%5==2||i%5==3){
+        printf("%d\n",i);
+        }
+      i++;
+    }
+}
+return 0;
+}
+//another way
 #include <stdio.h>
 
 int main() {

009f1411c3996925e8a097e0d093e71b4b3969cb
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-22 23:57:35 +0600

    Update p15.c

--- p15.c
+++ p15.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main() {
-    int x,i,cnt;
-    scanf("%d", &x);
-    i=x;
-    cnt=0;
-    while(cnt<6){
-       if(i%2!=0) {
-        printf("%d\n", i);
-        cnt++;
-       }
-     i++;
-    }
+    int n, i,cnt;
+    scanf("%d", &n);
+    cnt=1;
+    i=n;
+    while (cnt <=6) {
+        if (i % 2 != 0) {
+            printf("%d\n", i);
+            cnt++;
+        }
+       i++;
+     }
     return 0;
 }

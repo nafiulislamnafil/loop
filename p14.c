59cf204ab4667497f98c083aa4fb49d0ce03316a
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-23 19:37:23 +0600

    Update p14.c

--- p14.c
+++ p14.c
@@ -1,1 +1,15 @@
-S
+#include <stdio.h>
+int main() {
+int a,i,b,gcd;
+    scanf("%d%d", &a,&b);
+    i=1;
+   while (i<=a&&i<=b) {
+        if(a%i==0&&b%i==0){
+                gcd=i;
+        }
+      i++;
+   }
+   printf("%d",gcd);
+    return 0;
+}
+

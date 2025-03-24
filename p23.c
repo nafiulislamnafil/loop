dd9ca92cd9305351aaa37a9ad24034fae8456206
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-24 22:39:19 +0600

    Update p23.c

--- p23.c
+++ p23.c
@@ -1,18 +1,43 @@
 #include <stdio.h>
 int main() {
-    int n;
+    int i , n;
     scanf("%d", &n);
-
-    while (n != 1) {
-        printf("%d ", n);
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = 3 * n + 1;
+        i = 1;
+    while (i > 0) {
+        if (n == 1) {
+            printf("%d ", n);
+            break;
         }
+        if (n % 2 != 0) {
+            printf("%d ", n);
+             n = 3 * n + 1;
+        } 
+        else {
+            printf("%d ", n);
+            n = n / 2;
+        }
+        i++;
     }
-    printf("%d\n", n);
-
     return 0;
 }
-
+//another way
+#include <stdio.h>
+int main() {
+int ,n;
+    scanf("%d",&n);
+   while (n>=1) {
+        if(n==1){
+              printf("%d ",n);
+              break;
+        }
+        if(n%2!=0){
+            printf("%d ",n);
+            n=3*n+1;
+         }
+         if (n%2==0){
+            printf("%d ",n);
+            n=n/2;
+                }
+   }
+    return 0;
+}

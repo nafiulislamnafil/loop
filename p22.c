7b2f23200b6fe0ba7f977a74ff9757276672a17b
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-24 19:39:37 +0600

    Update p22.c

--- p22.c
+++ p22.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int main() {
+int a,i,b,lcm;
+    scanf("%d%d", &a,&b);
+    i=1;
+   while (i!=0) {
+        if(i%a==0&&i%b==0){
+                lcm=i;
+                break;
+        }
+      i++;
+   }
+   printf("%d",lcm);
+    return 0;
+}

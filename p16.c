8adc85e75696106a8ce6902735b76a7f7c6d063c
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-26 23:52:19 +0600

    Update p16.c

--- p16.c
+++ p16.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 int main() {
-    int i,a,b,s;
-    scanf("%d%d", &a,&b);
-    i=1;
-    s=1;
-    while(i<=b){
-      s=s*a;
-     i++;
+long long int i,a,b,s;
+scanf("%lld%lld",&a,&b);
+i=1;
+s=1;
+while(i<=b){
+        s=s*a;
+        i++;
     }
-    printf("%d",s);
-    return 0;
+printf("%lld ",s);
+return 0;
 }
+

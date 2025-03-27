3484f600ffb9addd19426b6fe9ffc4990ec57e67
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-28 00:08:50 +0600

    Update p20.c

--- p20.c
+++ p20.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 int main() {
-int n,a,i;
-    scanf("%d", &n);
-    i=1;
-    a=0;
-   while (i<n) {
-       if(n%2==0){
-        a=1;
-        break;
-        }
-      i++;
-     }
-     if(a==1){
-         printf("No");
-      }
-      else if(a!=1 ||n==1){
+int i,n,s;
+scanf("%d", &n);
+i=2;
+s=0;
+while(i<n){
+            if(n%i==0){
+                s=1;
+                break;
+            }
+        i++;
+ }
+if(s==1||n==1){
+            printf("No");
+}
+else if (s!=1) {
          printf("Yes");
-      }
-    return 0;
+}
+return 0;
 }

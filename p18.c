436cecb65dcbdeca8f21bc5474b0e12840a75965
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-27 23:35:27 +0600

    Update p18.c

--- p18.c
+++ p18.c
@@ -1,3 +1,31 @@
+///this is right one
+#include <stdio.h>
+int main() {
+    int n,i,ld;
+scanf("%d",&n);
+i=1;
+while(n!=0){     /////n=123
+ld=n%10;          //     3  2 1
+      if(n<10){
+      printf("%d",ld);
+      break;
+    }
+     else if(n>=10){
+     n=n/10;          ///      12  1
+     printf("%d, ",ld);   // 3, 2, 1
+   }
+  i++;
+}
+return 0;
+}
+
+
+
+
+
+
+
+
 #include <stdio.h>
 int main() {
 int n,ld,s;

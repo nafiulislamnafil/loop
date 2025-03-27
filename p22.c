cc018c1f1b9e18301358d209aad2be1a98c9c7f5
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-28 01:43:16 +0600

    Update p22.c

--- p22.c
+++ p22.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 int main() {
+int i,a,b,s;
+scanf("%d%d", &a,&b);
+i=1;
+s=0;
+while(i!=0){
+        if(i%a==0&&i%b==0){
+                s=1;
+                break;
+        }
+    i++;
+ }
+ if(s==1){
+printf("%d",i);
+ }
+return 0;
+}
+another away
+#include <stdio.h>
+int main() {
 int a,i,b,lcm;
     scanf("%d%d", &a,&b);
     i=1;

bc27dcd69e8eef8f9acc3d0ceeda3e6713652ab0
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-29 22:15:50 +0600

    Update p24.c

--- p24.c
+++ p24.c
@@ -1,19 +1,14 @@
 #include <stdio.h>
 int main() {
-    int i,n,a,cnt,s;
-    scanf("%d", &n);
-    i=1;
-    cnt = 0;
-    s = 0;
-    while(i<=n){
-            scanf("%d", &a);
-            cnt++;
-            if(cnt<=n){
-                s=s*0+a;        //not use it
-                printf("%d",s);// printf("%d",a); output will be
-            }                                                           // same
-         i++;
-     }
-    printf("%d",s*s);
-    return 0;
+int i,n,a,s;
+scanf("%d", &n);//n=4
+i=1;
+s=0;
+while(i<=n){//i=1 2 3 4
+       scanf("%d", &a); //a=1 3 4 2 
+       s=s*10+a;//s=1,13,134,1342
+       i++;
+}
+printf("%d",s*s);
+return 0;
 }

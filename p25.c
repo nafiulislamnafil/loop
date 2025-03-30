6b91bdcab5e256994cf9a136045166dd5326d19e
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-03-30 20:41:02 +0600

    Update p25.c

--- p25.c
+++ p25.c
@@ -1,17 +1,18 @@
-#include <stdio.h>
-int main() {
-    int n, ld,s,multi;
-    s = 0;
-    multi = 1;
-    scanf("%d", &n);
-
-    while (n != 0) {
-        ld = n % 2;
-        n = n / 2;
-        s = s + ld * multi;
-        multi *= 10;
-    }
-
-    printf("%d\n", s);
-    return 0;
+#include<stdio.h>
+int main(){
+    int i,x,n,s,multi;
+    scanf("%d",&n);
+    i=1;
+    s=0;
+    multi=1;
+while(n!=0){//n=         4              ,2,                 1       0
+    x=n%2;//x=              0,             0,                   1
+    s=s+x*multi;//s=       0,           0,                 100 
+    multi=multi*10;//multi=10,     100
+     n=n/2;//n=                2,            1,                  0
+    i++;
 }
+printf("%d",s);
+return 0;
+}
+

aec96fbd3093cc044d55dce7cb1234843f605f1f
nafiulislamnafil <nafiulislamnafil@gmail.com>
2025-04-30 08:46:05 +0600

    Update p19.c

--- p19.c
+++ p19.c
@@ -3,11 +3,11 @@ int main() {
 int n,ld,s;
 scanf("%d", &n);
 s=0;
-    while(n!=0){                     //12345 ,1234,123 ,12  ,1 , 0 X
-        ld=n%10;                   //5            ,4       ,3     ,2   ,1
-        n=n/10;                 //1234            ,123   ,12  ,1   ,0
-s=s*0+ld;//s=ld;   //5                         ,4       ,3    ,2   ,1
-        printf("%d,",s);
-    }
+    while(n!=0){
+        ld=n%10;
+        n=n/10;
+        s=s+ld;
+     }
+  printf("%d",s);
 return 0;
 }
